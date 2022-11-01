20fa0071ff351d1d54ae7c491e404bdce4fb48f4
Deny <flutterlabz@gmail.com>
2022-11-01 18:29:23 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:fhe_template/setup.dart';
 import 'package:fhe_template/state_util.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   await initialize();
 
   Widget mainView = Container();
-  if (FirebaseAuth.instance.currentUser != null) {
-    mainView = Container();
-  }
+  // if (FirebaseAuth.instance.currentUser != null) {
+  //   mainView = Container();
+  // }
 
   runApp(
     MaterialApp(
